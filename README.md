e9e42c1b20421824f11eee1a055139765963c501
vlad-mois <ijlil@yandex.ru>
2022-01-24 18:24:25 +0300

    Initial commit
    Updated example
    
    Updated AUTHORS and README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# Toloka-Prefect
+
+This library allows you to run crowdsourcing [Toloka](https://toloka.ai/) processes in [Prefect](https://www.prefect.io/) - a widely used workflow management system
+
+Here you can find a collection of ready-made Prefect tasks for the most frequently used actions in [Toloka-Kit](https://github.com/Toloka/toloka-kit).
+
+Getting started
+--------------
+```
+$ pip install toloka-prefect
+```
+
+A good way to start is to follow the [example](https://github.com/Toloka/toloka-prefect/blob/main/example/text_classification.ipynb) in this repo.
+
+Useful Links
+--------------
+- [Toloka homepage.](https://toloka.ai/)
+- [Prefect homepage.](https://prefect.io/)
+- [Toloka API documentation.](https://yandex.com/dev/toloka/doc/concepts/about.html?lang=en)
+- [Toloka-kit usage examples.](https://github.com/Toloka/toloka-kit/tree/main/examples#toloka-kit-usage-examples)
+
+Questions and bug reports
+--------------
+* For reporting bugs please use the [Toloka/bugreport](https://github.com/Toloka/toloka-prefect/issues) page.
+* Join our English-speaking [slack community](https://toloka.ai/community) for both tech and abstract questions.
+
+License
+-------
+© YANDEX LLC, 2020-2021. Licensed under the Apache License, Version 2.0. See LICENSE file for more details.
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='toloka-prefect',
+    packages=['toloka_prefect'],
+    version='0.0.1',
+    description='Toloka prefect tasks library',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='Apache 2.0',
+    author='Vladislav Moiseev',
+    author_email='vlad-mois@yandex-team.ru',
+    python_requires='>=3.7.0',
+    install_requires=[
+        'attrs',
+        'pandas >= 1.1.0',
+        'prefect',
+        'requests',
+        'toloka-kit>=0.1.21',
+    ],
+    include_package_data=True,
+    project_urls={
+        'Source': 'https://github.com/Toloka/toloka-prefect',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Typing :: Typed',
+    ],
+)
